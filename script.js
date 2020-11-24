672591122992e34cfc584cca96ca4968e83568ca
wahyuamirulloh <amirullohw@gmail.com>
2020-11-24 16:34:09 +0700

    Improving code quality until 107 line

--- script.js
+++ script.js
@@ -4,25 +4,13 @@ var o = new Array();
 var player_versus = 'player';
 var is_game_over = false;
 
-levelChooser = (nameEnemy) => {
-	let playerEnemy;
-	if (nameEnemy == 'human'){
-		playerEnemy = 'player';
-	} else if (nameEnemy == 'AI - Easy') {
-		playerEnemy = 'tic_AI';
-	} else if (nameEnemy == 'AI - Medium') {
-		playerEnemy = 'tac_AI';
-	} else {
-		playerEnemy = 'player';
-	}
-	document.getElementById('popup-level').classList.toggle('hide');
-	return playerEnemy;
-}
-
+// Listen what user choose for its enemy
 playerChooseLevel = () => {
-	player_versus = levelChooser(event.target.attributes.name.value);
+	player_versus = event.target.attributes.name.value;
+	document.getElementById('popup-level').classList.toggle('hide');
 }
 
+// Check the board
 checkArray = (turn) => {
 	console.log(`value of O : ${o}`);
 	console.log(`value of X : ${x}`);
@@ -80,206 +68,230 @@ checkArray = (turn) => {
 	}
 }
 
-tic_AI = () => {
-	apply_col = (check_col, random_choose) => {
-		if (check_col.classList.length == 1){
-			check_col.innerHTML = turn;
-			check_col.classList.add(`${turn.toLowerCase()}`);
-			o.push(`${random_choose}`);
-			let isPlayerWin = checkArray(o);
-			if (isPlayerWin == 'win'){
-				alert(`O Win`);
-				is_game_over = true;
-				document.getElementById('retry_button').classList.toggle('hide');
-			} else if (isPlayerWin == 'tie') {
-				alert('It\'s a Tie')
-				is_game_over = true;
-				document.getElementById('retry_button').classList.toggle('hide');
-			}
+// Check the column is it column empty
+getColumnInformation = (column) => {
+	let getColumn = document.getElementById(column);
+	if (getColumn.classList.length == 1) {
+		return getColumn;
+	}
+}
+
+// Generate random number between 0 - 9
+numberRandomer = () => {
+	return Math.ceil(Math.random()*9);
+}
+
+columnApplier = (columnInformation) => {
+	columnInformation.innerHTML = turn;
+	columnInformation.classList.add(`${turn.toLowerCase()}`);
+}
+
+checkIfPlayerWin = (turn) => {
+	let isPlayerWin = checkArray(turn);
+	if (isPlayerWin == 'win'){
+		alert(`O Win`);
+		is_game_over = true;
+		document.getElementById('retry_button').classList.toggle('hide');
+	} else if (isPlayerWin == 'tie') {
+		alert('It\'s a Tie')
+		is_game_over = true;
+		document.getElementById('retry_button').classList.toggle('hide');
+	}
+}
+
+easyAI = () => {
+	apply_col = () => {
+		let columnInformation = getColumnInformation(numberRandomer());
+		console.log(columnInformation);
+		if (columnInformation.classList.length == 1){
+			// Add javascript error handle here if column is > 1
+			columnApplier(columnInformation);
+			o.push(`${columnInformation.id}`);
+			checkIfPlayerWin('o')
 			turn = 'X';
 		} else {
 			if (is_game_over == false){
-				random_num();
+				numberRandomer();
 			}
 		}
 	}
-	random_num = () => {
-		let random_choose = Math.ceil(Math.random()*9);
-		let check_col = document.getElementById(random_choose);
-		if (is_game_over == false){
-			apply_col(check_col, random_choose);
-		}
-	}
-	random_num();
+	apply_col();
 	console.log('Goes here 114');
 }
 
-var o_strategy = new Array();
-tac_AI = () => {
-	apply_col = (blocked_col) => {
-		if (document.getElementById(blocked_col).classList.length == 1) {
-			let col_block = document.getElementById(`${blocked_col}`);
-			col_block.innerHTML = turn;
-			col_block.classList.add(`${turn.toLowerCase()}`);
-			o.push(`${blocked_col}`);
-			if (checkArray(o) == 'win') {
-				alert('O Win');
-				is_game_over = true;
-				document.getElementById('retry_button').classList.toggle('hide');
-			}
-			turn = 'X';
-		} else {
-			check_center();
-		}
-	}
-	blocked_num = ()  => {
-		random_num = () => {
-			let num = Math.ceil(Math.random()*9);
-			return num;
-		}
-		if (o_strategy.includes('1')) {
-			if (o_strategy.includes('2')) {
-				if (!o_strategy.includes('3')) {
-					o_strategy = [];
-					return '3';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('4')) {
-				if (!o_strategy.includes('7')) {
-					o_strategy = [];
-					return '7';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('3')) {
-				if (!o_strategy.includes('2')) {
-					o_strategy = [];
-					return '2';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('7')) {
-				if (!o_strategy.includes('4')) {
-					o_strategy = [];
-					return '4';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('5')) {
-				if (!o_strategy.includes('9')) {
-					o_strategy = [];
-					return '9';
-				} else {
-					random_num();
-				}
-			} else {
-				return random_num();
-			}
-		} else if (o_strategy.includes('5')) {
-			if (o_strategy.includes('2')) {
-				if (!o_strategy.includes('8')) {
-					o_strategy = [];
-					return '8';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('3')) {
-				if (!o_strategy.includes('7')) {
-					o_strategy = [];
-					return '7';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('4')) {
-				if (!o_strategy.includes('6')) {
-					o_strategy = [];
-					return '6';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('6')) {
-				if (!o_strategy.includes('4')) {
-					o_strategy = [];
-					return '4';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('7')) {
-				if (!o_strategy.includes('3')) {
-					o_strategy = [];
-					return '3';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('8')) {
-				if (!o_strategy.includes('2')) {
-					o_strategy = [];
-					return '2';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('9')) {
-				if (!o_strategy.includes('5')) {
-					o_strategy = [];
-					return '1';
-				} else {
-					random_num();
-				}
-			} else {
-				return random_num();
-			}
-		} else if (o_strategy.includes('9')) {
-			if (o_strategy.includes('6')) {
-				if (!o_strategy.includes('3')) {
-					o_strategy = [];
-					return '3';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('8')) {
-				if (!o_strategy.includes('7')) {
-					o_strategy = [];
-					return '7';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('3')) {
-				if (!o_strategy.includes('6')) {
-					o_strategy = [];
-					return '6';
-				} else {
-					random_num();
-				}
-			} else if (o_strategy.includes('7')) {
-				if (!o_strategy.includes('8')) {
-					o_strategy = [];
-					return '8';
-				} else {
-					random_num();
-				}
-			} else {
-				return random_num();
-			}
-		} else {
-			return random_num();
-		}
-	}
-	check_center = (numblock) => {
-		if (is_game_over == false){
-			if (numblock == undefined){
-				numblock = Math.ceil(Math.random()*9);
-			}
-			var randomednum = numblock;
-			if (document.getElementById(randomednum).classList.length > 1){
-				apply_col(Math.ceil(Math.random()*9));
-			} else {
-				apply_col(randomednum);
-			}
-		}
-	}
-check_center(blocked_num());
-}
+// var o_strategy = new Array();
+// mediumAI = () => {
+// 	console.log('Goes here 107');
+// 	apply_col = (blocked_col) => {
+// 		console.log('Goes here 109');
+// 		if (document.getElementById(blocked_col).classList.length == 1) {
+// 			let col_block = document.getElementById(`${blocked_col}`);
+// 			col_block.innerHTML = turn;
+// 			col_block.classList.add(`${turn.toLowerCase()}`);
+// 			o.push(`${blocked_col}`);
+// 			console.log('Goes here 113');
+// 			if (checkArray(o) == 'win') {
+// 				alert('O Win');
+// 				is_game_over = true;
+// 				document.getElementById('retry_button').classList.toggle('hide');
+// 			} else if (checkArray(o) == 'tie') {
+// 				alert('It\'s a Tie');
+// 				is_game_over = true;
+// 				document.getElementById('retry_button').classList.toggle('hide');
+// 			}
+// 			turn = 'X';
+// 		} else {
+// 			check_center();
+// 		}
+// 	}
+// 	blocked_num = ()  => {
+// 		random_num = () => {
+// 			let num = Math.ceil(Math.random()*9);
+// 			return num;
+// 		}
+// 		if (o_strategy.includes('1')) {
+// 			if (o_strategy.includes('2')) {
+// 				if (!o_strategy.includes('3')) {
+// 					o_strategy = [];
+// 					return '3';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('4')) {
+// 				if (!o_strategy.includes('7')) {
+// 					o_strategy = [];
+// 					return '7';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('3')) {
+// 				if (!o_strategy.includes('2')) {
+// 					o_strategy = [];
+// 					return '2';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('7')) {
+// 				if (!o_strategy.includes('4')) {
+// 					o_strategy = [];
+// 					return '4';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('5')) {
+// 				if (!o_strategy.includes('9')) {
+// 					o_strategy = [];
+// 					return '9';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else {
+// 				return random_num();
+// 			}
+// 		} else if (o_strategy.includes('5')) {
+// 			if (o_strategy.includes('2')) {
+// 				if (!o_strategy.includes('8')) {
+// 					o_strategy = [];
+// 					return '8';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('3')) {
+// 				if (!o_strategy.includes('7')) {
+// 					o_strategy = [];
+// 					return '7';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('4')) {
+// 				if (!o_strategy.includes('6')) {
+// 					o_strategy = [];
+// 					return '6';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('6')) {
+// 				if (!o_strategy.includes('4')) {
+// 					o_strategy = [];
+// 					return '4';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('7')) {
+// 				if (!o_strategy.includes('3')) {
+// 					o_strategy = [];
+// 					return '3';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('8')) {
+// 				if (!o_strategy.includes('2')) {
+// 					o_strategy = [];
+// 					return '2';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('9')) {
+// 				if (!o_strategy.includes('5')) {
+// 					o_strategy = [];
+// 					return '1';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else {
+// 				return random_num();
+// 			}
+// 		} else if (o_strategy.includes('9')) {
+// 			if (o_strategy.includes('6')) {
+// 				if (!o_strategy.includes('3')) {
+// 					o_strategy = [];
+// 					return '3';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('8')) {
+// 				if (!o_strategy.includes('7')) {
+// 					o_strategy = [];
+// 					return '7';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('3')) {
+// 				if (!o_strategy.includes('6')) {
+// 					o_strategy = [];
+// 					return '6';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else if (o_strategy.includes('7')) {
+// 				if (!o_strategy.includes('8')) {
+// 					o_strategy = [];
+// 					return '8';
+// 				} else {
+// 					random_num();
+// 				}
+// 			} else {
+// 				return random_num();
+// 			}
+// 		} else {
+// 			return random_num();
+// 		}
+// 	}
+// 	check_center = (numblock) => {
+// 		if (is_game_over == false){
+// 			if (numblock == undefined){
+// 				numblock = Math.ceil(Math.random()*9);
+// 			}
+// 			var randomednum = numblock;
+// 			if (document.getElementById(randomednum).classList.length > 1){
+// 				apply_col(Math.ceil(Math.random()*9));
+// 			} else {
+// 				apply_col(randomednum);
+// 			}
+// 		}
+// 	}
+// check_center(blocked_num());
+// }
 
 col_click = () => {
 	if (is_game_over == false) {
@@ -304,10 +316,10 @@ col_click = () => {
 					}
 					turn = 'O';
 				}
-			} else if (player_versus == 'tic_AI'){
+			} else if (player_versus == 'easyAI'){
 				if (is_game_over == false) {
 					if (turn == 'O') {
-						tic_AI();
+						easyAI();
 						turn = 'X';
 						console.log('Goes here 307');
 					} else {
@@ -324,15 +336,15 @@ col_click = () => {
 							console.log('Goes here 321');
 						}
 						turn = 'O';
-						tic_AI();
+						easyAI();
 						console.log('Goes here 322');
 					}
 				}
-			} else if (player_versus == 'tac_AI'){
+			} else if (player_versus == 'mediumAI'){
 				if (is_game_over == false) {
 					if (turn == 'O') {
 						turn = 'X';
-						tac_AI();
+						mediumAI();
 					} else {
 						x.push(event.target.id);
 						o_strategy.push(event.target.id);
@@ -342,7 +354,7 @@ col_click = () => {
 							document.getElementById('retry_button').classList.toggle('hide');
 						}
 						turn = 'O';
-						tac_AI();
+						mediumAI();
 					}
 				}
 			}
@@ -365,3 +377,5 @@ retry_button = () => {
 	turn = 'X';
 	document.getElementById('retry_button').classList.toggle('hide');
 }
+
+console.log('ready');
